42dd42f953bb6db0a734d421ace5ac766b499504
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-25 16:41:27 -0600

    📅  | Sat Jun 25 16:41:27 MDT 2022

--- parse-pdf/src/app/components/register-form/register-form.component.ts
+++ parse-pdf/src/app/components/register-form/register-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ParsePdfService } from 'src/app/core/services/parse-pdf.service';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  styleUrls: ['./register-form.component.scss'],
 })
 export class RegisterFormComponent implements OnInit {
-
-  constructor() { }
+  constructor(public parsePdfService: ParsePdfService) {}
 
   ngOnInit(): void {
+    this.parsePdfService.parsedInfo$.subscribe((data: any) =>
+      console.log(data)
+    );
   }
-
 }
